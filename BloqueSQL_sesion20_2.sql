72d94895d22fe0b81be6092c53925be8e83c45c4
MCantuarias <m.cantuarias@alumnos.ucentral.cl>
2026-06-17 15:44:03 -0400

    lab/ Se agrega la parte 2 de la sesion 20, se crea tabla de auditorias con trigger para actualizar

---
+++ BloqueSQL_sesion20_2.sql
@@ -0,0 +1,20 @@
+-- Crear tabla de auditoría
+CREATE TABLE AuditoriaPedidos (
+	AuditoriaID NUMBER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+	PedidoID NUMBER,
+	ClienteID NUMBER,
+	Total NUMBER,
+	FechaEliminacion DATE
+);
+-- Crear trigger
+CREATE OR REPLACE TRIGGER auditar_eliminacion_pedido
+AFTER DELETE ON Pedidos
+FOR EACH ROW
+BEGIN
+	INSERT INTO AuditoriaPedidos (PedidoID, ClienteID, Total, FechaEliminacion)
+	VALUES (:OLD.PedidoID, :OLD.ClienteID, :OLD.Total, SYSDATE);
+END;
+/
+-- Prueba
+DELETE FROM Pedidos WHERE PedidoID = 102;
+SELECT * FROM AuditoriaPedidos;
